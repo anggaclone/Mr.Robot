3875c1ee1e905846e101e38bdd1f41fe4dbf5339
Angga <keep.hustling00@gmail.com>
2020-04-21 00:13:35 +0700

    ok ready to deploy

---
+++ build/precache-manifest.473a8554284ffbea19cad9801dcb6cf1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc1f4d43ce73a11369360dc953cf6c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f3d40de43b22a10f1a30",
+    "url": "/static/css/main.e97ba73e.chunk.css"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d40de43b22a10f1a30",
+    "url": "/static/js/main.c53b8211.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
